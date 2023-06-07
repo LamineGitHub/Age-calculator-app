96acbcbf86157f5de4bc1f7514608c39d6d8194d
Lamine Diallo <bah07565@gmail.com>
2023-06-07 01:09:53 +0100

    feat: Restrict form inputs to numeric values

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,21 @@
 import "../style/style.scss"
 import { calculateAge } from "./calculateAge"
 
+const formElement = document.querySelector("form")!
+
+// Ajout d'un écouteur d'événement pour restreindre l'entrée aux valeurs numériques
+formElement.addEventListener("input", (event) => {
+  const target = event.target as HTMLInputElement
+
+  if (target.type === "text") {
+    const inputValue = target.value
+    const numericValue = inputValue.replace(/\D/g, "")
+    target.value = numericValue
+  }
+})
+
 // Ajouter l'événement de soumission du formulaire
-document.querySelector("form")!.addEventListener("submit", (event) => {
+formElement.addEventListener("submit", (event) => {
   event.preventDefault()
   calculateAge()
 })
