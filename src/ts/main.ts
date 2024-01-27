29ea5459c8109ddc29eb053d7d3836a7eed7c576
Lamine Diallo <bah07565@gmail.com>
2024-01-27 07:07:46 +0000

    refactor: remove the input listener for a string typing

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,8 @@
 import "../style/style.scss"
 import { calculateAge } from "./calculateAge"
 
-const formElement = document.querySelector("form")!
-
-// Ajout d'un écouteur d'événement pour restreindre l'entrée aux valeurs numériques
-formElement.addEventListener("input", (event) => {
-  const target = event.target as HTMLInputElement
-
-  if (target.type === "text") {
-    const inputValue = target.value
-    target.value = inputValue.replace(/\D/g, "")
-  }
-})
-
-// Ajouter l'événement de soumission du formulaire
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault()
-  calculateAge()
-})
+document.querySelector("form")!
+  .addEventListener("submit", (e) => {
+    e.preventDefault()
+    calculateAge()
+  })
